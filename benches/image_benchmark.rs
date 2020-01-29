db3c43d93eb5dec4771fd1f0aaa00118d2da2a40
Mathieu <mathieu.0598@gmail.com>
2020-01-29 00:23:39 +0000

    Add bench for external lib rad and write ppm file function

--- benches/image_benchmark.rs
+++ benches/image_benchmark.rs
@@ -1,5 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use ppm::image::Image;
+use ppm::image::extern_ppm_lib;
 use ppm::pixel::Pixel;
 use std::path::Path;
 use std::time::Duration;
@@ -39,6 +40,34 @@ fn bench_image_read(c: &mut Criterion) {
     std::fs::remove_file(path).expect("Finally try to delete the file");
 }
 
-criterion_group!(benches, bench_image_write, bench_image_read);
+fn bench_external_lib_image_write(c: &mut Criterion) {
+
+    let image = get_image();
+    let path = Path::new("image_bench.ppm");
+
+    let mut group = c.benchmark_group("benches write with external lib");
+    group.measurement_time(Duration::new(20, 0));
+    group.sample_size(50);
+    group.bench_function("bench save image struct with external lib", |b| b.iter(|| extern_ppm_lib::write(black_box(path.to_str().unwrap()), black_box(&image))));
+    group.finish();
+
+    std::fs::remove_file(path).expect("Finally try to delete the file");
+}
+
+fn bench_external_lib_image_read(c: &mut Criterion) {
+
+    let image = get_image();
+    let path = Path::new("image_bench.ppm");
+    extern_ppm_lib::write(path.to_str().unwrap(), &image);
+
+    let mut group = c.benchmark_group("benches read");
+    group.measurement_time(Duration::new(20, 0));
+    group.bench_function("bench read and get image struct with external lib", |b| b.iter_with_large_drop(|| extern_ppm_lib::read(black_box(path.to_str().unwrap()))));
+    group.finish();
+
+    std::fs::remove_file(path).expect("Finally try to delete the file");
+}
+
+criterion_group!(benches, bench_image_write, bench_image_read, bench_external_lib_image_write, bench_external_lib_image_read);
 
 criterion_main!(benches);
